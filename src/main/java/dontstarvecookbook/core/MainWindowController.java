38deeec5dfacae3041496e1d6516bd425671e6be
Maciej <gorski.maciej@o2.pl>
2020-08-26 16:09:27 +0200

    Added displaying basic dish info like name, hunger, health and sanity restored. Removed 'target' directory

--- src/main/java/dontstarvecookbook/core/MainWindowController.java
+++ src/main/java/dontstarvecookbook/core/MainWindowController.java
@@ -9,6 +9,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -18,9 +19,13 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 public class MainWindowController implements Initializable {
 
@@ -89,23 +94,61 @@ public class MainWindowController implements Initializable {
     }
 
     private void initializeListViewCellFactory() {
-
+        dishesListView.setCellFactory(callback -> new ListCell<>() {
+            private ImageView view = new ImageView();
+            @Override
+            protected void updateItem(CrockPotDish item, boolean empty) {
+                super.updateItem(item, empty);
+                if (!isEmpty()) {
+                    var filename = item.getName().toLowerCase().replace(' ', '-').replace("'", "");
+                    String path = String.format("/images/crockpot-dish-icons/%s.png", filename);
+                    view.setImage(new Image(getClass().getResource(path).toExternalForm()));
+                    setGraphic(view);
+                    setText(item.getName());
+                } else {
+                    setGraphic(null);
+                    setText(null);
+                }
+            }
+        });
     }
 
     private void initializeListViewContents() {
-
+        dishesListView.getItems().addAll(CrockPotDishesStorage.getInstance().getDishes());
     }
 
     private void initializeListViewEvents() {
+        dishesListView.setOnMouseClicked(e -> {
+            var item = dishesListView.getSelectionModel().getSelectedItem();
+            if (item != null) displayDishInformation(item);
+        });
+    }
 
+    private void displayDishInformation(CrockPotDish dish) {
+        var df = new DecimalFormat();
+        df.setMaximumFractionDigits(1);
+        var dfs = new DecimalFormatSymbols();
+        dfs.setDecimalSeparator('.');
+        df.setDecimalFormatSymbols(dfs);
+        dishNameLabel.setText(dish.getName());
+        healthRestoredLabel.setText(Integer.toString(dish.getHealthRecovered()));
+        hungerRestoredLabel.setText(df.format(dish.getHungerRecovered()));
+        sanityRestoredLabel.setText(Integer.toString(dish.getSanityRecovered()));
+
+        var filename = dish.getName().toLowerCase().replace(' ', '-').replace("'", "");
+        var path = String.format("/images/crockpot-dish-icons/%s.png", filename);
+        dishIconImageView.setImage(new Image(getClass().getResource(path).toExternalForm()));
+
+        //TODO: fill the infoVBox with labels describing dish information
     }
 
     private List<CrockPotDish> gatherDishesByType(DishType type) {
-        return null;
+        return CrockPotDishesStorage.getInstance().getDishes().
+                stream().filter(dish -> dish.getDishType().equals(type)).collect(Collectors.toList());
     }
 
     @FXML
-    private void showFoodValuesWindow(ActionEvent e) {
+    public void showFoodValuesWindow(ActionEvent e) {
         try {
             Parent p = FXMLLoader.load(getClass().getResource("/dontstarvecookbook/fxml/foodvalueswindow.fxml"));
             Scene s = new Scene(p);
@@ -113,9 +156,7 @@ public class MainWindowController implements Initializable {
             stage.initModality(Modality.NONE);
             stage.setTitle("Food values");
             stage.setScene(s);
-            try (InputStream is = getClass().getResourceAsStream("/images/appicon.png")) {
-                stage.getIcons().add(new Image(is));
-            }
+            stage.getIcons().add(new Image(getClass().getResource("/images/appicon.png").toExternalForm()));
             stage.show();
         } catch (IOException ex) {
             ex.printStackTrace();
